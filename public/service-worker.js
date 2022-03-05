452149dc8adace38bbac0d1c810f9ceedabcddec
Jenny Keene <jennifer0keene@gmail.com>
2022-03-04 23:35:39 -0600

    Update service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -7,9 +7,14 @@ const FILES_TO_CACHE = [
     './css/styles.css',
     './js/idb.js',
     './js/index.js',
-    '../models/transaction.js',
-    '../routes/api.js',
-    '../server.js'
+    './icons/icon-128x128.png',
+    './icons/icon-144x144.png',
+    './icons/icon-152x152.png',
+    './icons/icon-192x192.png',
+    './icons/icon-384x384.png',
+    './icons/icon-512x512.png',
+    './icons/icon-72x72.png',
+    './icons/icon-96x96.png'
 ];
 
 // respond with cached resources when fetch request received
@@ -30,3 +35,4 @@ self.addEventListener('fetch', function(e) {
         })
     );
 });
+
